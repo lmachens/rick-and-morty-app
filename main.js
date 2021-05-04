11dce16f39993677deaf32d1e529c52273db60c7
lmachens <leon@machens.koeln>
2021-05-04 11:51:22 +0200

    ✨ Add page layout

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 import './style.css';
+import { createElement } from './utils/elements';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+const mainElement = createElement('main', {
+  className: 'main',
+  children: [
+    createElement('input', { placeholder: 'Enter name' }),
+    createElement('section', { innerText: 'Results' }),
+  ],
+});
+
+document.querySelector('#app').append(mainElement);

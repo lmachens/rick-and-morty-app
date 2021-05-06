e88e13dc3af296417d7dfa631acdf3f44742bf9f
phlgr <philipp@gartz.dev>
2021-05-06 13:52:02 +0200

    ✨ Create getCharcter function to fetch single Character

--- details.js
+++ details.js
@@ -1,9 +1,12 @@
 import './style.css';
 import styles from './details.module.css';
 import { createElement } from './utils/elements';
+import { getCharacter } from './utils/api';
 
 const params = new URLSearchParams(location.search);
 const characterId = params.get('id');
+
+getCharacter(characterId).then((response) => console.log(response));
 
 const mainElement = createElement('main', {
   className: styles.main,
